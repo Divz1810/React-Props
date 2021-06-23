8a7b22d2c08b25375e3931a6c6c3539060d6e8eb
Divz1810 <59832425+Divz1810@users.noreply.github.com>
2021-06-23 21:56:31 +0530

    3rd method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import contact from "../contacts";
-function Card(props){
-  return (<div className="card">
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      mail={contact.email}
+    />
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className ="circle-img" src= {props.img} alt="immg" />
+        <img className="circle-img" src={props.img} alt="immg" />
       </div>
-      <div className= "bottom">
+      <div className="bottom">
         <p className="info">{props.tel}</p>
         <p className="info">{props.mail}</p>
       </div>
-  </div>
+    </div>
   );
 }
 function App() {
@@ -18,49 +32,48 @@ function App() {
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-      name= "Beyonce"
-      img=
-        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      tel= "+123 456 789"
-      mail= "b@beyonce.com"
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        mail="b@beyonce.com"
       />
 
       <Card
-      name= "Jack Bauer"
-      img=
-        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      tel= "+987 654 321"
-      mail= "jack@nowhere.com"
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+987 654 321"
+        mail="jack@nowhere.com"
       />
 
       <Card
-      name= "Chuck Norris"
-      img=
-        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      tel= "+918 372 574"
-      mail= "gmail@chucknorris.com"
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="+918 372 574"
+        mail="gmail@chucknorris.com"
       />
 
       <Card
-      name= {contact[0].name}
-      img= {contact[0].imgURL}
-      tel= {contact[0].phone}
-      mail= {contact[0].email}
+        name={contact[0].name}
+        img={contact[0].imgURL}
+        tel={contact[0].phone}
+        mail={contact[0].email}
       />
 
       <Card
-      name= {contact[1].name}
-      img= {contact[1].imgURL}
-      tel= {contact[1].phone}
-      mail= {contact[1].email}
+        name={contact[1].name}
+        img={contact[1].imgURL}
+        tel={contact[1].phone}
+        mail={contact[1].email}
       />
 
-     <Card
-      name= {contact[2].name}
-      img= {contact[2].imgURL}
-      tel= {contact[2].phone}
-      mail= {contact[2].email}
+      <Card
+        name={contact[2].name}
+        img={contact[2].imgURL}
+        tel={contact[2].phone}
+        mail={contact[2].email}
       />
+
+      {contact.map(createCard)}
     </div>
   );
 }
